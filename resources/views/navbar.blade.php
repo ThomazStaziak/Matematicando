3f2c9c392639930d84aa47899aa3fde3eb1094ee
ThomazStaziak <thostaziak@gmail.com>
2018-08-21 15:01:15 -0300

    add footer

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav>
     <div class="nav-wrapper">
       <a href="#" class="brand-logo center">Matematicando</a>
-      <ul id="nav-mobile" class="left hide-on-med-and-down">
-        <li><a href="sass.html">Ranking</a></li>
-        <li><a href="badges.html">Login</a></li>
-        <li><a href="collapsible.html">FAQ</a></li>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#">Leaderboards</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">FAQ</a></li>
       </ul>
     </div>
   </nav>
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$title or 'Matematicando - Jogo de Inteligência'}}</title>
-    <link rel="stylesheet" href="../public/css/materialize.css">
+    <link rel="stylesheet" href="{{ url('css/materialize.css') }}">
 </head>
 <body>
     @include('navbar')
     <div class="container">
         @yield('section')
+        <h1>Teste</h1>
+        <h1>Teste</h1>
+        <h1>Teste</h1>
     </div>
-    {{--@include('footer')--}}
+    @include('footer')
 </body>
 </html>
