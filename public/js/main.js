3528bc5591f6fdfcc9fbd5005a9e8f19fcf54a25
ThomazStaziak <thostaziak@gmail.com>
2018-08-24 01:42:49 -0300

    fix timer system and add score system

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,18 @@
 const form = document.querySelector('#form')
 const input = document.querySelector('#numero')
-const background = document.body;
+const background = document.body
+const time = 40
 let counter = 0
-
-
+let tempoResposta = 0
+let quantidadeCertas = 0
+let quantidadeErradas = 0
+let pontuacao = 0
 
 function sleep(milliseconds) {
-  var start = new Date().getTime();
+  var start = new Date().getTime()
   for (let i = 0; i < 1e7; i++) {
     if ((new Date().getTime() - start) > milliseconds){
-      break;
+      break
     }
   }
 }
@@ -19,71 +22,65 @@ function plugHtml(id, algo) {
 }
 
 function mostrarPergunta() {
-  plugHtml('pergunta', array[counter]['questao']);
+  plugHtml('pergunta', array[counter]['questao'])
   counter++
-}
-
-function cronometro(segundos) {
-  let novo = segundos
-  const interval = setInterval(() => {
-    if (segundos < 10 && segundos >= 0) {
-      plugHtml('tempo', '00:0' + segundos)
-    } else {
-      plugHtml('tempo', '00:' + segundos)
-    }
-    if (segundos <= 0) {
-     // console.log('teste');
-      
-      piscarTela('red')
-      clearInterval(interval)
-      mostrarPergunta()
-      setTimeout(() => { piscarTela('white');},800)
-      cronometro(novo)
-    }
-    segundos--
-  }, 1000)
 }
 
 function piscarTela(cor) {
   background.style.backgroundColor = cor
 }
 
+function cronometro(segundos, parar) {
+  const interval = setInterval(() => {
+    if (segundos < 10 && !parar) {
+      tempoResposta = segundos
+      plugHtml('pontuacao', pontuacao)
+      plugHtml('tempo', '00:0' + segundos)
+    } else {
+      tempoResposta = segundos
+      plugHtml('pontuacao', pontuacao)
+      plugHtml('tempo', '00:' + segundos)
+    }
+    if (segundos <= 0 || parar) {
+      piscarTela('red')
+      plugHtml('pergunta', array[5]['questao'])
+      clearInterval(interval)
+    }
+    segundos--
+  }, 1000)
+}
 input.addEventListener('keypress', (evento) => {
   if (evento.charCode === 13) {
-
-    let resposta_certa = array[counter-1]['resposta'];
+    let resposta_certa = array[counter - 1]['resposta'];
     let resposta = evento.srcElement.value;
-    
+    evento.srcElement.value = ""
     if(resposta == resposta_certa){
+      if (tempoResposta >= 30) {
+        pontuacao += 1000
+      } else if (tempoResposta >= 20) {
+        pontuacao += 800
+      } else if (tempoResposta >= 10) {
+        pontuacao += 600
+      } else if (tempoResposta < 10) {
+        pontuacao += 400
+      }
       piscarTela('green')
-      setTimeout(() => { piscarTela('white'),mostrarPergunta()},800)
-      evento.srcElement.value = ""
-      cronometro(novo)
-    }else {
+      setTimeout(() => {
+        piscarTela('white')
+        mostrarPergunta()
+        plugHtml('pontuacao', pontuacao)
+      }, 400)
+    } else {
+      pontuacao -= 300
       piscarTela('red')
-      clearInterval(interval)
-      mostrarPergunta()
-      setTimeout(() => { piscarTela('white');},800)
-      cronometro(novo)
+      setTimeout(() => {
+        piscarTela('white')
+        mostrarPergunta()
+        plugHtml('pontuacao', pontuacao)
+      }, 400)
     }
-    
-    
   }
 })
-// function cronometro(segundos) {
-//   let novo = segundos
-//   const interval = setInterval(() => {
-//     if (segundos < 10) {
-//       plugHtml('tempo', "00:0" + segundos)
-//     } else {
-//       plugHtml('tempo', "00:" + segundos)
-//     }
-//     if (segundos <= 0) {
-//       clearInterval(interval)
-//     }
-//     segundos--
-//   }, 1000)
-// }
 
 mostrarPergunta()
-cronometro(15)
+cronometro(time, false)
--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -6,19 +6,24 @@
               <div class="col s8 offset-s2">
                 <div class="col s3 left-align">
                   <h5>Tempo</h5>
-                  <span id="tempo" class="timer"></span>
+                  <span id="tempo" class="numero"></span>
                 </div>
                 <div class="col s6 center-align">
                   <h5>Quanto é?</h5>
-                  <h1 id="pergunta"></h1>
+                  <h1 id="pergunta" class="numero"></h1>
                   <div class="col s2 offset-s5">
-                      <input type="text" class="center-align" name="numero" id="numero">
+                    <input type="hidden" name="" value="">
+                    <input type="text" class="center-align" name="numero" id="numero">
                   </div>
+                </div>
+                <div class="col s3 center-align">
+                  <h5>Pontuação</h5>
+                  <h6 id="pontuacao" class="numero"></h6>
                 </div>
               </div>
             </div>
           </div>
-          
+
           <script type="text/javascript">
             const array = <?php echo json_encode($array) ?>
           </script>
