aa8110b25953320aff75841c69e8b79988bd5d60
Vinicius Oliveira <vinicius.developer@outlook.com>
2018-08-25 18:54:38 -0300

    Alter fonts in file

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="page-footer">
+<footer class="page-footer purple lighten-1">
   <div class="container">
     <p class="grey-text text-lighten-4 center"> © Copyright Matematicando </p>
   </div>
--- resources/views/template/navbar.blade.php
+++ resources/views/template/navbar.blade.php
@@ -1,6 +1,7 @@
-<nav>
+<nav class="purple lighten-1
+">
     <div class="nav-wrapper">
-      <a href="{{ url('/') }}" class="brand-logo center">Matematicando</a>
+      <a href="{{ url('/') }}" class="brand-logo center fonte-logo">Matematicando</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         @guest
           <li><a href="{{ url('login') }}">Jogar</a></li>
