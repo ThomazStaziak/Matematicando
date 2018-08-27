70cf142360b7d1bc36233cc982981ff1189b827a
ThomazStaziak <thostaziak@gmail.com>
2018-08-27 02:36:00 -0300

    add the comoJogar route

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/easy', 'ScoresController@easy');
+
+Route::get('/comoJogar', function(){
+  return view('como');
+});
